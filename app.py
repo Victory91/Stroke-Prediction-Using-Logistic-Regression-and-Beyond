df269c77dd195c7e865996c0065303d18f7cab32
Victory91 <99646928+Victory91@users.noreply.github.com>
2025-02-23 15:20:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ if st.button("Predict"):
       y_pred = model1.predict([[age, bmi, avg_glucose_level]])[0].round(0)
   # Map the prediction to the corresponding message
 if y_pred == 1:
-        status = "Stroke Event"
+    status = "Stroke Event"
 else:
-        status = "No Stroke Event"
+    status = "No Stroke Event"
 
 # Display the result on the Streamlit app
 st.write("The predicted stroke status is", status)
