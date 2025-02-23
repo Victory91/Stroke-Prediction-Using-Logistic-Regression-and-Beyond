0e9ba3f79d673a12afbde9bd7b8dfd58f9e41a0d
Victory91 <99646928+Victory91@users.noreply.github.com>
2025-02-23 15:11:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,13 +14,11 @@ st.title("Stroke Predictor")
 age = st.slider("age", min_value=0, max_value=100, value=0)
 bmi = st.slider("bmi", min_value=0, max_value=100, value=10)
 avg_glucose_level = st.slider("avg_glucose_level", min_value=0, max_value=500, value=50)
-
-
 # After selecting stroke, the user then submits the stroke value
 
 if st.button("Predict"):
   # take the stroke value, and format the value the right way
-        y_pred = model1.predict([[age, bmi, avg_glucose_level]])[0].round(0)
+      y_pred = model1.predict([[age, bmi, avg_glucose_level]])[0].round(0)
   # Map the prediction to the corresponding message
 if y_pred == 1:
         status = "Stroke Event"
